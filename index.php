95535eb47b979f7073db194f4016fc06edcd169d
Sidney Souza <sidneysase@gmail.com>
2015-04-20 09:06:58 -0300

    Adicionado redirect para evitar digitar index.php na url

--- index.php
+++ index.php
@@ -4,5 +4,9 @@
 
 require_once 'src/bootstrap.php';
 
+if (!preg_match('/index\.php/', $_SERVER['REQUEST_URI'])) {
+    header('location: index.php');
+}
+
 $system = new Core\core\System();
 $system->run();
