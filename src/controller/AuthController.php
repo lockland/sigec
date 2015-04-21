5197db9a15afc38ea61cf887e254de5e9e4d9e1c
Sidney Souza <sidneysase@gmail.com>
2015-04-21 09:30:24 -0300

    Passando pdo por referencia

--- src/controller/AuthController.php
+++ src/controller/AuthController.php
@@ -12,9 +12,16 @@ class AuthController extends Controller
     private $view = null;
     private $session = null;
     private $mainUrl = "";
+    private $pdo;
 
-    public function __construct()
+    public function __construct(\PDO $pdo = null)
     {
+        $this->pdo = $pdo ?: new \PDO(
+            'mysql:host='.DB_HOST.';dbname='.DB_BASE, 
+            DB_USER, 
+            DB_PASS
+        );
+
         $this->session = new Session();
         $this->mainUrl = 'location: ' . URL_BASE . '/index.php/Main';
         $this->view = new Login();
@@ -30,8 +37,7 @@ class AuthController extends Controller
     {
         $this->isLogged();
 
-        $pdo = new \PDO('mysql:host='.DB_HOST.';dbname='.DB_BASE, DB_USER, DB_PASS);
-        $user = new User($pdo);
+        $user = new User($this->pdo);
 
         try {
             $user->retrieveByCredential($_POST['login'], $_POST['password']);
