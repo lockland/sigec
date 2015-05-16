af59cb3d0e756a99d6b9fec9653be25fd266d9c5
Sidney Souza <sidneysase@gmail.com>
2015-05-16 12:21:51 -0300

    Criado o backend para o crud de cliente.
    Criados as funções de adicionar, listar e editar
    o cliente através interface web.
    
    close #13

--- src/controller/ClientController.php
+++ src/controller/ClientController.php
@@ -4,10 +4,12 @@ namespace Sigec\controller;
 
 use Sigec\view\Client as View;
 use Core\helpers\Redirector;
+use Sigec\model\Client;
 
 class ClientController extends ControllerBase
 {
     private $view = null;
+    private $model = null;
 
     public function __construct()
     {
@@ -16,43 +18,82 @@ class ClientController extends ControllerBase
         $this->view->assign('action', '');
         $this->view->assign('controller', 'Client');
         $this->view->assign('username', $this->user->getName());
-    }
-
-    public function index()
-    {
-        $this->view->assign('clients', []);
-        $this->view->generateHTML();
+        $this->client = new Client(new \PDO(DB_DSN, DB_USER, DB_PASS));
     }
 
     public function listAll()
     {
-        $this->view->assign('clients', []);
+        $this->view->assign('clients', $this->client->fetchAll());
         $this->view->generateHTML();
     }
 
-    public function add($errors = [], $user = null)
+    public function add($errors = [], $client = null)
     {
         $this->view->assign('errors', $errors);
+        $this->view->assign('client', $client);
         $this->view->assign('action', __FUNCTION__);
         $this->view->generateHTML();
     }
 
-    public function update($errors = [], $user = null)
+    public function update($errors = [], $client = null)
     {
-        $this->view->assign('errors', $errors);
-        $this->view->assign('action', __FUNCTION__);
-        $this->view->generateHTML();
-    }
+        $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+        $client = $client ?: new \StdClass();
 
-    public function delete()
-    {
+        try {
+            $this->client->retrieve($id);
+            $client->id = $this->client->getId();
+            $client->name = $this->client->getName();
+            $client->motherName = $this->client->getMotherName();
+            $client->address = $this->client->getAddress();
+            $client->phone = $this->client->getPhone();
+            $client->cpfOrCnpj = $this->client->getCpfOrCnpj();
+            $client->email = $this->client->getEmail();
+            $client->city = $this->client->getCity();
+            $client->state = $this->client->getState();
+            $client->district = $this->client->getDistrict();
+
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+            $errors[] = 'Could not retrieve client using id';
+        }
+
+        $this->view->assign('errors', $errors);
+        $this->view->assign('client',$client);
+        $this->view->assign('action', __FUNCTION__);
         $this->view->generateHTML();
     }
 
     public function save()
     {
-        $this->view->assign('clients', []);
-        $this->view->generateHTML();
+        $errors = [];
+        $post = (object) $_POST;
+
+        try {
+            $this->client->setId((int) $post->id);
+            $this->client->setName($post->name);
+            $this->client->setMotherName($post->motherName);
+            $this->client->setAddress($post->address);
+            $this->client->setPhone($post->phone);
+            $this->client->setCpfOrCnpj($post->cpfOrCnpj);
+            $this->client->setEmail($post->email);
+            $this->client->setCity($post->city);
+            $this->client->setState($post->state);
+            $this->client->setDistrict($post->district);
+            $this->client->save();
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+            $errors[] = 'Could not store the client in database';
+        }
+
+        $action = $_GET['action'];
+        
+        if (count($errors) <= 0) {
+            $redirector = new Redirector('Client', 'listAll');
+            $redirector->redirect();
+        }
+
+        $this->$action($errors, $post);
     }
 
     public function filter()
